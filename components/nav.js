df22e0e444eb1099628e37d1fceaf4cfe306e6c7
Sam Poder <sam.r.poder@gmail.com>
2022-11-24 12:50:43 +0800

    Add Deprecation Notice

--- components/nav.js
+++ components/nav.js
@@ -1,24 +1,31 @@
 /**@jsxImportSource theme-ui*/
 
 import Image from "next/image";
-import { Container, Button } from "theme-ui";
+import { Container, Button, Box, Link } from "theme-ui";
 
 import flag_src from "../public/flag.png";
 
 const Nav = ({ ...props }) => {
   return (
-    <Container {...props} sx={{ display: "flex", alignItems: "center" }}>
-      <a href="https://leaderboard.hackclub.com">
-       <Image src={flag_src} width={72 * 1.5} height={40 * 1.5} />
-      </a>
-      <Button
-        as="a"
-        href="https://hackclub.com"
-        sx={{ display: "inline-block", ml: "auto", px: [4] }}
-      >
-        join
-      </Button>
-    </Container>
+    <>
+      <Box bg="red" sx={{ fontWeight: "bold" }}>
+        APAC Directory is being phased out in favour of Scrapbook. Join #scrapbook on {" "}
+        <Link href="https://hackclub.com/slack">the Hack Club Slack</Link> {" "}
+        to get started or <Link href="mailto:zach@hackclub.com">reach out</Link> for more details.
+      </Box>
+      <Container {...props} sx={{ display: "flex", alignItems: "center" }}>
+        <a href="https://leaderboard.hackclub.com">
+         <Image src={flag_src} width={72 * 1.5} height={40 * 1.5} />
+        </a>
+        <Button
+          as="a"
+          href="https://hackclub.com"
+          sx={{ display: "inline-block", ml: "auto", px: [4] }}
+        >
+          join
+        </Button>
+      </Container>
+    </>
   );
 };
 
